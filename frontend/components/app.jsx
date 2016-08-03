28be82aa75f79d88ff3989083d6cb4c28a2bd144
Kei Man Yeung <ky2286@columbia.edu>
2016-08-03 09:57:40 -0400

    add log out button, display current user

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,16 +1,58 @@
 const React = require('react');
 const Link = require('react-router').Link;
+const SessionStore = require('../stores/session_store');
+const SessionActions = require('../actions/session_actions');
 
 module.exports = React.createClass({
+  getInitialState() {
+    return { isUserLoggedIn: SessionStore.isUserLoggedIn() };
+  },
+
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+
+  componentDidMount() {
+    this.listener = SessionStore.addListener(this._onChange);
+  },
+
+  componentWillUnmount() {
+    this.listener.remove();
+  },
+
+  _onChange() {
+    this.setState({ isUserLoggedIn: SessionStore.isUserLoggedIn() });
+  },
+
+  _handleLogout(e) {
+    e.preventDefault();
+    SessionActions.logout();
+  },
+
+  _toDisplay() {
+    if (this.state.isUserLoggedIn) {
+      return (
+        <div>
+          { SessionStore.currentUser() }
+          <button onClick={this._handleLogout}>Logout</button>
+        </div>
+      );
+    } else {
+      return(
+        <div>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link>
+        </div>
+      );
+    }
+  },
+
   render() {
     return(
       <div>
-        <div>
-          <Link to="/login">Login</Link>
-        </div>
-        <div>
-          <Link to="/signup">Sign Up</Link>
-        </div>
+        <header>
+          { this._toDisplay() }
+        </header>
         {this.props.children}
       </div>
     );
