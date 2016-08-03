1e4cbdcc974fc306f52a0bb082f2e6b31a5883f9
Kei Man Yeung <ky2286@columbia.edu>
2016-08-02 22:15:06 -0400

    add error actions

---
+++ frontend/actions/error_actions.js
@@ -0,0 +1,19 @@
+const SessionApiUtil = require('../util/session_api_util');
+const ErrorConstants = require('../constants/error_constants');
+const AppDispatcher = require('../dispatcher/dispatcher');
+
+module.exports = {
+  setErrors(errors, form) {
+    AppDispatcher.dispatch({
+      actionType: ErrorConstants.CLEAR_ERRORS,
+      errors: errors,
+      form: form
+    });
+  },
+
+  clearErrors() {
+    AppDispatcher.dispatch({
+      actionType: ErrorConstants.CLEAR_ERRORS,
+    });
+  }
+};
