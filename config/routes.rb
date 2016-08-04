3332c7d886aa897fd30def375530720f3d0e4c54
Kei Man Yeung <ky2286@columbia.edu>
2016-08-04 15:47:38 -0400

    update routes with posts resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root "static_pages#root"
-  
+
   namespace :api do
+    resources :posts, only: [:create, :index, :show, :update, :destroy]
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy, :show]
   end
