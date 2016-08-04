0d67b457ab4f183bae58171453576c40edefa76f
Kei Man Yeung <ky2286@columbia.edu>
2016-08-04 14:53:37 -0400

    initial posts controller methods

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,36 @@
 class Api::PostsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(
+      :type,
+      :title,
+      :body,
+      :photo_url,
+      :link_url,
+      :audio_url,
+      :video_url,
+      :author_id
+    )
+  end
 end
