0e373d6f154cc5f1ce5caf4305a7e8ed465cb396
Kei Man Yeung <ky2286@columbia.edu>
2016-08-04 16:24:48 -0400

    update update, destroy action, and post_params with post_type

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,7 +2,7 @@ class Api::PostsController < ApplicationController
   # before action require sign in
   # before ction require user be owner
   def index
-
+    @posts = Post.all
   end
 
   def show
@@ -21,7 +21,7 @@ class Api::PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    if @post.update
+    if @post.update(post_params)
       render json: @post
     else
       render json: @post.errors.full_messages, status: 400
@@ -31,13 +31,14 @@ class Api::PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
+    render json: {}
   end
 
   private
 
   def post_params
     params.require(:post).permit(
-      :type,
+      :post_type,
       :title,
       :body,
       :photo_url,
