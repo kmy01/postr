e414ff54d4b6f5878e5039a92d3d4cbef7beb72f
Kei Man Yeung <ky2286@columbia.edu>
2016-08-04 15:32:22 -0400

    edit post controller actions

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,22 +1,36 @@
 class Api::PostsController < ApplicationController
+  # before action require sign in
+  # before ction require user be owner
   def index
 
   end
 
   def show
-
+    @post = Post.find(params[:id])
   end
 
   def create
-
+    @post = Post.create(post_params)
+    @post.author_id = current_user.id
+    if @post.save
+      render json: @post
+    else
+      render json: @post.errors.full_messages, status: 400
+    end
   end
 
-  def edit
-
+  def update
+    @post = Post.find(params[:id])
+    if @post.update
+      render json: @post
+    else
+      render json: @post.errors.full_messages, status: 400
+    end
   end
 
   def destroy
-
+    @post = Post.find(params[:id])
+    @post.destroy
   end
 
   private
