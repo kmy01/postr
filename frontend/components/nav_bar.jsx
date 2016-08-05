5b07fafd22e2841d177226ebddfe1ed5f37780ce
Kei Man Yeung <ky2286@columbia.edu>
2016-08-05 10:21:27 -0400

    create initial nav bar component

---
+++ frontend/components/nav_bar.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+
+module.exports = React.createClass({
+  render() {
+    return (
+      <div>
+        <div> a logo link to home </div>
+        <div> a search bar component</div>
+        <div> link to home with house icon </div>
+        <div> explore link </div>
+        <div> Account DropDown </div>
+      </div>
+    );
+  }
+});
