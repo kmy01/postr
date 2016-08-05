8ddcfa9761117d5e7ad73f1e5f28230bd7cd26f6
Kei Man Yeung <ky2286@columbia.edu>
2016-08-04 22:57:11 -0400

    add post validations and association to user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+  validates :post_type, :author_id, presence: true
+  validates :post_type, inclusion: { in: %w(text photo link audio video) }
+
+  belongs_to :author,
+    class_name: :User,
+    foreign_key: :author_id,
+    primary_key: :id
 end
