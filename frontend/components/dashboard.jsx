92d68cc785138afb16fbf45a6da963d06253461f
Kei Man Yeung <ky2286@columbia.edu>
2016-08-05 16:30:05 -0400

    add photo form component

--- frontend/components/dashboard.jsx
+++ frontend/components/dashboard.jsx
@@ -2,12 +2,19 @@ const React = require('react');
 const PostFormBar = require('./posts/post_form_bar');
 const NavBar = require('./nav_bar');
 
+const TextForm = require('./posts/text_form/text_form');
+const PhotoForm = require('./posts/photo_form/photo_form');
+
 module.exports = React.createClass({
   render() {
     return (
       <div>
         <NavBar />
         <PostFormBar />
+        <br/>
+        <TextForm />
+        <br/>
+        <PhotoForm />
       </div>
     );
   }
