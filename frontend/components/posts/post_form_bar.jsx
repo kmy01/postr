39411753fbf30c25af1ff97e653eaa93abd5e34e
Kei Man Yeung <ky2286@columbia.edu>
2016-08-05 10:21:52 -0400

    create initial post form bar component

---
+++ frontend/components/posts/post_form_bar.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+
+module.exports = React.createClass({
+  render() {
+    return (
+      <ul>
+        <li>Text</li>
+        <li>Photo</li>
+        <li>Link</li>
+        <li>Audio</li>
+        <li>Video</li>
+      </ul>
+    );
+  }
+});
