df7ee6ac30de23e8c553e9de7a5187abb1a43d2a
Kei Man Yeung <ky2286@columbia.edu>
2016-08-05 20:28:12 -0400

    add more content_types

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,8 +10,8 @@ class Post < ActiveRecord::Base
   # has_attached_file :media_content, default_url: "/images/:style/missing.png"
   has_attached_file :media_content
   validates_attachment_content_type :media_content,
-    content_type: ['image/jpeg','image/png','audio/mpeg','video/mp4','video/avi']
+    content_type: ['image/jpeg','image/png','audio/mpeg','audio/x-m4a','video/mp4','video/avi']
   validates_attachment_file_name :media_content,
-    matches: [/png\Z/i, /jpe?g\Z/i, /mp3\Z/i, /mp4\Z/i, /avi\Z/i]
+    matches: [/png\Z/i, /jpe?g\Z/i, /m4a\Z/i, /mp3\Z/i, /mp4\Z/i, /avi\Z/i]
 
 end
