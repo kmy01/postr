f2ec485c49f14d6719faf57d0a89a402661543a3
Kei Man Yeung <ky2286@columbia.edu>
2016-08-06 19:12:54 -0400

    fix render partial line

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,7 +3,7 @@ class Api::UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in(@user)
-      render partial: 'api/users/user', user: @user
+      render partial: 'api/users/user', object: @user
     else
       render json: @user.errors.full_messages, status: 422
     end
@@ -12,6 +12,6 @@ class Api::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :avatar)
   end
 end
