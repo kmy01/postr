62216bb43497c361c5a0e634f52c2ad7ec0b7e94
Kei Man Yeung <ky2286@columbia.edu>
2016-08-05 20:35:47 -0400

    add working link form

---
+++ frontend/components/posts/link_form/link_form.jsx
@@ -0,0 +1,57 @@
+const React = require('react');
+const PostActions = require('../../../actions/post_actions');
+const SessionStore = require('../../../stores/session_store');
+
+module.exports = React.createClass({
+  getInitialState() {
+    return {
+      linkUrl: '',
+      body: ''
+    };
+  },
+
+  componentDidMount() {
+    //store listener
+  },
+
+  componentWillUnmount() {
+    //remove listeners
+  },
+
+  _onSubmit(e) {
+    e.preventDefault();
+    const postData = new FormData();
+    postData.append('post[author_id]', SessionStore.currentUser().id);
+    postData.append('post[post_type]', 'link');
+    postData.append('post[link_url]', this.state.linkUrl);
+    postData.append('post[body]', this.state.body);
+
+    PostActions.createPost(postData);
+    this.setState({ linkUrl: '', body: '' });
+  },
+
+  _onLinkChange(e) {
+    this.setState({ linkUrl: e.target.value });
+  },
+
+  _onBodyChange(e) {
+    this.setState({ body: e.target.value });
+  },
+
+  render() {
+    return (
+      <form>
+        <input
+          placeholder="Link"
+          value={this.state.linkUrl}
+          type="url"
+          onChange={this._onLinkChange} />
+        <textarea
+          value={this.state.body}
+          onChange={this._onBodyChange} />
+        <button
+          onClick={this._onSubmit}>Post</button>
+      </form>
+    );
+  }
+});
