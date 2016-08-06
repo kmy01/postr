2b2f3782861c1ee49b29d315746b67bab4589f2c
Kei Man Yeung <ky2286@columbia.edu>
2016-08-06 16:51:42 -0400

    create post feed component

---
+++ frontend/components/posts/post_feed.jsx
@@ -0,0 +1,38 @@
+const React = require('react');
+const SessionStore = require('../../stores/session_store');
+const PostStore = require('../../stores/post_store');
+const PostActions = require('../../actions/post_actions');
+const PostFeedItem = require('./post_feed_item');
+
+module.exports = React.createClass({
+  getInitialState() {
+    return({ posts: PostStore.all() });
+  },
+
+  componentDidMount() {
+    this.listener = PostStore.addListener(this._onChange);
+    PostActions.fetchAllPosts();
+  },
+
+  _onChange() {
+    this.setState({ posts: PostStore.all() });
+  },
+
+  componentWillUnmount() {
+    this.listener.remove();
+  },
+
+  render() {
+    let postItems = this.state.posts.map((post) => {
+      return(
+        <li key={post.id}><PostFeedItem post={post} /></li>
+      );
+    });
+
+    return(
+      <ul>
+        { postItems }
+      </ul>
+    );
+  }
+});
