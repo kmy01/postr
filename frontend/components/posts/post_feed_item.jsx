17ecd1c7c05dff083514f9da3da5de135acabfe2
Kei Man Yeung <ky2286@columbia.edu>
2016-08-06 16:52:06 -0400

    create post feed item component

---
+++ frontend/components/posts/post_feed_item.jsx
@@ -0,0 +1,89 @@
+const React = require('react');
+const SessionStore = require('../../stores/session_store');
+const PostConstants = require('../../constants/post_constants');
+
+module.exports = React.createClass({
+  _postToRender() {
+    let post = this.props.post;
+    let postRender;
+
+    switch (post.post_type) {
+      case PostConstants.TEXT:
+        postRender = this._textPost(post);
+        break;
+      case PostConstants.PHOTO:
+        postRender = this._photoPost(post);
+        break;
+      case PostConstants.LINK:
+        postRender = this._linkPost(post);
+        break;
+      case PostConstants.AUDIO:
+        postRender = this._audioPost(post);
+        break;
+      case PostConstants.VIDEO:
+        postRender = this._videoPost(post);
+        break;
+    }
+
+    return postRender;
+  },
+
+  _textPost(post) {
+    return(
+      <div>
+        <h2>{ post.title }</h2>
+        <div>{ post.body }</div>
+      </div>
+    );
+  },
+
+  _photoPost(post) {
+    return(
+      <div>
+        <img className='photo-post' src={post.photo_url} />
+        <div>{ post.body }</div>
+      </div>
+    );
+  },
+
+  _linkPost(post) {
+    return(
+      <div>
+        <a href={post.link_url}>{ post.link_url }</a>
+        <div>{ post.body }</div>
+      </div>
+    );
+  },
+
+  _audioPost(post) {
+    return(
+      <div>
+        <audio
+          className='audio-post'
+          controls
+          src={post.audio_url} />
+        <div>{ post.body }</div>
+      </div>
+    );
+  },
+
+  _videoPost(post) {
+    return(
+      <div>
+        <video
+          className='video-post'
+          src={post.video_url} />
+        <div>{ post.body }</div>
+      </div>
+    );
+  },
+
+  render() {
+    return(
+      <div>
+        { this.props.post.author.username }
+        { this._postToRender() }
+      </div>
+    );
+  }
+});
