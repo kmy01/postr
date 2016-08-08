c49fecb6b988225a4b08b51a6bcf2601258697a4
Kei Man Yeung <ky2286@columbia.edu>
2016-08-08 15:37:52 -0400

    create modal stytle for forms

---
+++ frontend/components/posts/post_form_modal_style.js
@@ -0,0 +1,21 @@
+module.exports = {
+  overlay : {
+    position        : 'fixed',
+    top             : 0,
+    left            : 0,
+    right           : 0,
+    bottom          : 0,
+    backgroundColor : 'rgba(54,70,93,.95)',
+    zIndex          : 10
+  },
+  content : {
+    position        : 'fixed',
+    maxWidth        : '540px',
+    height          : '500px',
+    top             : '55px',
+    left            : '96px',
+    padding         : '0',
+    border          : '1px solid #ccc',
+    zIndex          : 11
+  }
+};
