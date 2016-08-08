fb504a44b410792282c657a800f13fe6bfcfc830
Kei Man Yeung <ky2286@columbia.edu>
2016-08-08 15:35:21 -0400

    move all the form types to for post formbar

--- frontend/components/dashboard.jsx
+++ frontend/components/dashboard.jsx
@@ -1,15 +1,10 @@
 const React = require('react');
-const PostFormBar = require('./posts/post_form_bar');
-const NavBar = require('./nav_bar');
 
 const SessionActions = require('../actions/session_actions');
 const SessionStore = require('../stores/session_store');
 
-const TextForm = require('./posts/text_form/text_form');
-const PhotoForm = require('./posts/photo_form/photo_form');
-const LinkForm = require('./posts/link_form/link_form');
-const AudioForm = require('./posts/audio_form/audio_form');
-const VideoForm = require('./posts/video_form/video_form');
+const PostFormBar = require('./posts/post_form_bar');
+const NavBar = require('./nav_bar');
 
 const PostFeed = require('./posts/post_feed');
 
@@ -35,11 +30,6 @@ module.exports = React.createClass({
         </header>
         <main className='main'>
           <PostFormBar />
-          <TextForm />
-          <PhotoForm />
-          <LinkForm />
-          <AudioForm />
-          <VideoForm />
           <PostFeed />
         </main>
       </div>
