15d3ae8eadcd42c2a66d428f29701a41facc8b78
Kei Man Yeung <ky2286@columbia.edu>
2016-08-08 13:37:37 -0400

    add classNames

--- frontend/components/nav_bar.jsx
+++ frontend/components/nav_bar.jsx
@@ -1,15 +1,54 @@
 const React = require('react');
 
+const SessionActions = require('../actions/session_actions');
+const SessionStore = require('../stores/session_store');
+
 module.exports = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+
+  componentDidMount() {
+    this.sessionListener = SessionStore.addListener(this._onChange);
+  },
+
+  componentWillUnmount() {
+    this.sessionListener.remove();
+  },
+
+  _onChange() {
+    if (!SessionStore.isUserLoggedIn()) {
+      this.context.router.push('/');
+    }
+  },
+
+  _handleLogout(e) {
+    e.preventDefault();
+    SessionActions.logout();
+  },
+
   render() {
     return (
-      <div>
-        <div> a logo link to home </div>
-        <div> a search bar component</div>
-        <div> link to home with house icon </div>
-        <div> explore link </div>
-        <div> Account DropDown </div>
-      </div>
+      <nav className='nav-bar group'>
+        <div className='logo'> a logo </div>
+        <div className='search-bar'> this is a search bar </div>
+
+        <ul className='nav-list'>
+          <li><a href='/dashboard'>Home</a></li>
+          <li><a href='#'>Explore</a></li>
+          <li className='account'>
+            <div>Account</div>
+            <ul className='account-list'>
+              <li>{ SessionStore.currentUser().username }</li>
+              <li><a href='#'>Edit Profile</a></li>
+              <li><a href='#'>Posts</a></li>
+              <li><a href='#'>Likes</a></li>
+              <li><a href='#'>Following</a></li>
+              <li onClick={this._handleLogout}>Logout</li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
     );
   }
 });
