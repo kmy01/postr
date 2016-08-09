7668d13c302e6c08ca45125b9d2a69b95cdd28c9
Kei Man Yeung <ky2286@columbia.edu>
2016-08-09 09:52:15 -0400

    finish like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,13 @@
 class Like < ActiveRecord::Base
+  validates :post_id, :user_id, presence: true
+
+  belongs_to :post,
+    class: 'Post',
+    foreign_key: :post_id,
+    primary_key: :id
+
+  belongs_to :user,
+    class: 'User',
+    foreign_key: :user_id,
+    primary_key: :id
 end
