f1497e22e8cd829007946b302f77b21ba6d01aa0
Kei Man Yeung <ky2286@columbia.edu>
2016-08-10 11:23:02 -0400

    add default json

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#root"
 
-  namespace :api do
+  namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy, :show]
     resources :users, only: [:create]
     resources :posts, only: [:create, :index, :show, :update, :destroy]
