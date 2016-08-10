c8aeecf402fe3d0141f67561a46c73a400db256b
Kei Man Yeung <ky2286@columbia.edu>
2016-08-10 12:15:22 -0400

    add function for add and remove follow for users

--- frontend/stores/session_store.js
+++ frontend/stores/session_store.js
@@ -2,6 +2,7 @@ const AppDispatcher = require('../dispatcher/dispatcher');
 const Store = require('flux/utils').Store;
 const SessionConstants = require('../constants/session_constants');
 const LikeConstants = require('../constants/like_constants');
+const FollowConstants = require('../constants/follow_constants');
 
 const SessionStore = new Store(AppDispatcher);
 let _currentUser = {};
@@ -24,13 +25,32 @@ SessionStore.isUserLoggedIn = function () {
   return !!_currentUser.id;
 };
 
-SessionStore._removeLikedPost = function(like) {
+SessionStore._removeLikedPost = function (like) {
   _currentUser.liked_posts.forEach((post, i) => {
     post.likes.forEach((likeEl) => {
       if (likeEl.id === like.id) {
         _currentUser.liked_posts.splice(i, 1);
       }
     });
+  });
+  this.__emitChange();
+};
+
+SessionStore._addFollow = function (follow) {
+  _currentUser.liked_posts.forEach((post) => {
+    if (post.author.id === follow.followee_id) {
+      post.author.followers.push(_currentUser.id);
+    }
+  });
+  this.__emitChange();
+};
+
+SessionStore._removeFollow = function (follow) {
+  _currentUser.liked_posts.forEach((post) => {
+    if (post.author.id === follow.followee_id) {
+      const idx = post.author.followers.indexOf(_currentUser.id);
+      post.author.followers.splice(idx, 1);
+    }
   });
   this.__emitChange();
 };
@@ -45,6 +65,12 @@ SessionStore.__onDispatch = function (payload) {
       break;
     case LikeConstants.LIKE_REMOVED:
       this._removeLikedPost(payload.like);
+      break;
+    case FollowConstants.FOLLOW_RECEIVED:
+      this._addFollow(payload.follow);
+      break;
+    case FollowConstants.FOLLOW_REMOVED:
+      this._removeFollow(payload.follow);
       break;
   }
 };
