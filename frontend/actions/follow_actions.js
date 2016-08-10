6fe086d4d3a4ea13321a9547120e74407060ddc3
Kei Man Yeung <ky2286@columbia.edu>
2016-08-10 10:18:26 -0400

    add follow actions

---
+++ frontend/actions/follow_actions.js
@@ -0,0 +1,28 @@
+const AppDispatcher = require('../dispatcher/dispatcher');
+const FollowApiUtil = require('../util/follow_api_util');
+const FollowConstants = require('../constants/follow_constants');
+const ErrorActions = require('../actions/error_actions');
+
+module.epxorts ={
+  createFollow(followData) {
+    FollowApiUtil.createFollow(followData, this.receiveFollow, ErrorActions.setErrors);
+  },
+
+  deleteFollow(id) {
+    FollowApiUtil.deleteFollow(id, this.removeFollow, ErrorActions.setErrors);
+  },
+
+  receiveFollow(follow) {
+    AppDispatcher.dispatch({
+      actionType: FollowConstants.FOLLOW_RECEIVED,
+      follow: follow
+    });
+  },
+
+  removeFollow(follow) {
+    AppDispatcher.dispatch({
+      actionType: FollowConstants.FOLLOW_REMOVED,
+      follow: follow
+    });
+  }
+};
