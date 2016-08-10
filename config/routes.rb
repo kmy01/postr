aabe44a1ffe479c19ce4f98d085e087a9080acd3
Kei Man Yeung <ky2286@columbia.edu>
2016-08-10 11:28:51 -0400

    change to resource for follows

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :users, only: [:create]
     resources :posts, only: [:create, :index, :show, :update, :destroy]
     resources :likes, only: [:index, :create, :destroy]
-    resources :follows, only: [:create, :destroy]
+    resource :follows, only: [:create, :destroy]
   end
 
   get '*path', to: 'static_pages#root'
