e71dd2b4d24e6435bbd5f2cffb0168942af6fa91
Kei Man Yeung <ky2286@columbia.edu>
2016-08-09 21:11:32 -0400

    add remove like

--- frontend/stores/session_store.js
+++ frontend/stores/session_store.js
@@ -1,6 +1,7 @@
 const AppDispatcher = require('../dispatcher/dispatcher');
 const Store = require('flux/utils').Store;
 const SessionConstants = require('../constants/session_constants');
+const LikeConstants = require('../constants/like_constants');
 
 const SessionStore = new Store(AppDispatcher);
 let _currentUser = {};
@@ -16,11 +17,22 @@ SessionStore._logout = function () {
 };
 
 SessionStore.currentUser = function () {
-  return { id: _currentUser.id, username: _currentUser.username, avatar_url: _currentUser.avatar_url };
+  return { id: _currentUser.id, username: _currentUser.username, avatar_url: _currentUser.avatar_url, liked_posts: _currentUser.liked_posts};
 };
 
 SessionStore.isUserLoggedIn = function () {
   return !!_currentUser.id;
+};
+
+SessionStore._removeLikedPost = function(like) {
+  _currentUser.liked_posts.forEach((post, i) => {
+    post.likes.forEach((likeEl) => {
+      if (likeEl.id === like.id) {
+        _currentUser.liked_posts.splice(i, 1);
+      }
+    });
+  });
+  this.__emitChange();
 };
 
 SessionStore.__onDispatch = function (payload) {
@@ -30,6 +42,9 @@ SessionStore.__onDispatch = function (payload) {
       break;
     case SessionConstants.LOGOUT:
       this._logout();
+      break;
+    case LikeConstants.LIKE_REMOVED:
+      this._removeLikedPost(payload.like);
       break;
   }
 };
