73708ba0ed2f866152dce07368a7991de764441e
Kei Man Yeung <ky2286@columbia.edu>
2016-08-11 09:36:22 -0400

    add tagging validations and associations

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,2 +1,14 @@
 class Tagging < ActiveRecord::Base
+  validates :post, :tag, presence: true
+  validates :tag_id, uniqueness: { scope: :post_id }
+
+  belongs_to :post,
+    class_name: 'Post',
+    foreign_key: :post_id,
+    primary_key: :id
+
+  belongs_to :tag,
+    class_name: 'Tag',
+    foreign_key: :tag_id,
+    primary_key: :id
 end
