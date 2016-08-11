c97ae98f2b0af21672056704485e6d3ecb8dccab
Kei Man Yeung <ky2286@columbia.edu>
2016-08-11 12:22:11 -0400

    create tagged component

---
+++ frontend/components/tagged.jsx
@@ -0,0 +1,46 @@
+const React = require('react');
+
+const NavBar = require('./nav_bar');
+
+const SessionStore = require('../stores/session_store');
+const PostFeed = require('./posts/post_feed');
+
+const PostStore = require('../stores/post_store');
+const PostActions = require('../actions/post_actions');
+
+module.exports = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+
+  getInitialState() {
+    return({ posts: PostStore.all() });
+  },
+
+  componentDidMount() {
+    this.listener = PostStore.addListener(this._onChange);
+    PostActions.fetchTaggedPosts(this.props.params.tagName);
+  },
+
+  componentWillUnmount() {
+    this.listener.remove();
+  },
+
+  _onChange() {
+    this.setState({ posts: PostStore.all() });
+  },
+
+  render() {
+    debugger
+    return (
+      <div>
+        <header className='header-nav group'>
+          <NavBar />
+        </header>
+        <main className='main'>
+          <PostFeed posts={this.state.posts}/>
+        </main>
+      </div>
+    );
+  }
+});
