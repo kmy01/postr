ec72d631c6adca1808fc3117b7a038166ba90c62
Kei Man Yeung <ky2286@columbia.edu>
2016-08-12 09:55:06 -0400

    create explore component

---
+++ frontend/components/explore.jsx
@@ -0,0 +1,46 @@
+const React = require('react');
+
+const NavBar = require('./nav_bar');
+const PostFormBar = require('./posts/post_form_bar');
+
+const PostFeed = require('./posts/post_feed');
+const PostStore = require('../stores/post_store');
+const PostActions = require('../actions/post_actions');
+
+const SessionStore = require('../stores/session_store');
+
+module.exports = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+
+  getInitialState() {
+    return({ posts: PostStore.all() });
+  },
+
+  componentDidMount() {
+    this.postListener = PostStore.addListener(this._onPostChange);
+    PostActions.fetchAllPosts();
+  },
+
+  componentWillUnmount() {
+    this.postListener.remove();
+  },
+
+  _onPostChange() {
+    this.setState({ posts: PostStore.all() });
+  },
+
+  render() {
+    return (
+      <div>
+        <header className='header-nav group'>
+          <NavBar />
+        </header>
+        <main className='main'>
+          <PostFeed posts={this.state.posts}/>
+        </main>
+      </div>
+    );
+  }
+});
