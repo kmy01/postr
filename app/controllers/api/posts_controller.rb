fa12ed44cfeb7a5df9e370a9744e9366d7f8dfc9
Kei Man Yeung <ky2286@columbia.edu>
2016-08-12 09:36:24 -0400

    index returns different post depending on pathname

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -2,7 +2,11 @@ class Api::PostsController < ApplicationController
   # before action require sign in
   # before ction require user be owner
   def index
-    @posts = Post.all.includes(:likes, :tags, author: :followers)
+    if params[:pathname]
+      @posts = Post.joins(author: :followers).where("follower_id = ?", current_user.id)
+    else
+      @posts = Post.all.includes(:likes, :tags, author: :followers)
+    end
   end
 
   def show
