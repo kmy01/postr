30283400aed30ed5544d56e84e213696699604bb
Kei Man Yeung <ky2286@columbia.edu>
2016-08-16 19:10:03 -0400

    add eager load of likes and tags for dashboard path of index

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -3,7 +3,7 @@ class Api::PostsController < ApplicationController
   # before ction require user be owner
   def index
     if params[:pathname] == 'dashboard'
-      @posts = Post.joins(author: :followers).where("follower_id = ? OR author_id = ?", current_user.id, current_user.id)
+      @posts = Post.joins(author: :followers).where("follower_id = ? OR author_id = ?", current_user.id, current_user.id).includes(:likes, :tags)
     elsif params[:pathname] == 'likes'
       @posts = User.find(current_user.id).liked_posts
     else
